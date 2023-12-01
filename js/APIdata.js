4fa9847f80e91108ccf94fc9c549fe37c7fab7c0
Javier Alejandro Hernandez Sanchez <javier.hernandez7930@alumnos.udg.mx>
2023-12-01 16:30:39 -0600

    ADD: API usage and data show function
        added function to show api data usage
        added function to show specific data from JSON file

---
+++ js/APIdata.js
@@ -0,0 +1,10 @@
+export default function privacy(){
+    var API_KEY = "a3fbc0343da0419ead2c2539be68a35d";
+    var API_URL = "https://api.weatherbit.io/v2.0/current";
+    let lat = "20.656637665297726";
+    let lon = "-105.21483137274211";
+
+    let Data = [API_KEY, API_URL, lat, lon];
+
+    return Data;
+    }

299e955e76bb29cd9abea17ecc2e6de90c73e886
Javier Alejandro Hernandez Sanchez <javier.hernandez7930@alumnos.udg.mx>
2023-12-02 02:45:06 -0600

    REMOVE: API data files

--- js/APIdata.js
+++ js/APIdata.js
@@ -1,9 +1,9 @@
 export default function privacy(){
-    var API_KEY = "a3fbc0343da0419ead2c2539be68a35d";
-    var API_USAGE = "https://api.weatherbit.io/v2.0/subscription/usage?key=";
-    var API_URL = "http://api.weatherbit.io/v2.0/forecast/daily";
-    let lat = "20.656637665297726";
-    let lon = "-105.21483137274211";
+    var API_KEY = "";
+    var API_USAGE = "";
+    var API_URL = "";
+    let lat = "";
+    let lon = "";
     
 
     let Data = [API_KEY, API_URL, lat, lon, API_USAGE];
